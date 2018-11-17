1ef8f2bc47e61ec93fa2f8739bd208c77d1b24ce
apalmeida <apalmeida1990@gmail.com>
2018-11-17 01:37:34 +0000

    End of Aula105

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -43,6 +43,7 @@ namespace DatingWebApp.API
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySetttings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
